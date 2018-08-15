e17c8411780a426f21cc01a8890d92ff597e4fe9
Peter Diaz <pjsystem@gmail.com>
2018-08-15 15:51:52 -0500

    Adding elements for documentation flow

--- src/controllers/professions/createProfessions.js
+++ src/controllers/professions/createProfessions.js
@@ -1,5 +1,5 @@
 function createProfessions(req, res, next) {
-  res.status(200).json({ id: 1, name: 'profession 1' });
+  res.status(200).json({ id: 1, name: 'profession 1', state: 'FL' });
 }
 
 module.exports = createProfessions;
--- src/controllers/professions/getProfessions.js
+++ src/controllers/professions/getProfessions.js
@@ -1,7 +1,10 @@
 function getProfessions(req, res, next) {
   res
     .status(200)
-    .json([{ id: 1, name: 'profession 1' }, { id: 2, name: 'profession 2' }]);
+    .json([
+      { id: 1, name: 'profession 1', state: 'FL' },
+      { id: 2, name: 'profession 2', state: 'FL' }
+    ]);
 }
 
 module.exports = getProfessions;
